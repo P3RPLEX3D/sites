1ee05003a8ccfd8b9b8f36a7c728d0490a9afffe
Jacob Fillpot <admin@Jacobs-MacBook-Pro.local>
2019-12-14 16:39:40 -0600

    fixed id error

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ x.addEventListener('click', function prinit() {
 function Export2Doc(element, filename = ''){
     var preHtml = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
     var postHtml = "</body></html>";
-    var html = preHtml+document.getElementById(element).innerHTML+postHtml;
+    var html = preHtml+document.getElementById('demo').innerHTML+postHtml;
 
     var blob = new Blob(['\ufeff', html], {
         type: 'application/msword'
