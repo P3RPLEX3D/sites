7215720cfff3491c1c25e89fc5b484b307e5ad6e
Jacob Fillpot <admin@Jacobs-MacBook-Pro.local>
2019-12-14 17:37:16 -0600

    test

--- js/main.js
+++ js/main.js
@@ -7,3 +7,46 @@ localStorage.setItem('server', input);
 window.onload = function readValue() {
 document.getElementById('demo').innerHTML = localStorage.getItem('server');
 };
+
+
+var x = document.getElementById('button');
+x.addEventListener('click', function prinit() {
+	Export2Doc('exportedContent' ,'word-content')
+})
+
+
+function Export2Doc(_exportedContent, filename = ''){
+    var preHtml = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body>";
+    var postHtml = "</body></html>";
+    var html = preHtml+document.getElementById('exportedContent').innerHTML+postHtml;
+
+    var blob = new Blob(['\ufeff', html], {
+        type: 'application/msword'
+    });
+    
+    // Specify link url
+    var url = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+    
+    // Specify file name
+    filename = filename?filename+'.doc':'document.doc';
+    
+    // Create download link element
+    var downloadLink = document.createElement("a");
+
+    document.body.appendChild(downloadLink);
+    
+    if(navigator.msSaveOrOpenBlob ){
+        navigator.msSaveOrOpenBlob(blob, filename);
+    }else{
+        // Create a link to the file
+        downloadLink.href = url;
+        
+        // Setting the file name
+        downloadLink.download = filename;
+        
+        //triggering the function
+        downloadLink.click();
+    }
+    
+    document.body.removeChild(downloadLink);
+}
