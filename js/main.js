5efc00ac5bb9ce7054407eff582c8819f5640868
Jacob Fillpot <admin@Jacobs-MacBook-Pro.local>
2019-12-13 20:27:07 -0600

    new site

---
+++ js/main.js
@@ -0,0 +1,8 @@
+document.getElementById('demo').onkeypress = function createItem() {
+    var input = document.getElementById('demo').value;
+    localStorage.setItem('server', input);
+    }
+    
+    window.onload = function readValue() {
+    document.getElementById('demo').innerHTML = localStorage.getItem('server');
+    }
