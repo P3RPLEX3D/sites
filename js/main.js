f0aa0b281cfc8fea5ea00a2fc3d50a7b804f0535
Jacob Fillpot <admin@Jacobs-MacBook-Pro.local>
2019-12-13 22:35:39 -0600

    printer button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
+document.getElementById('button').onclick = function printMe() {
+	let x = document.getElementById('demo');
+	window.print(x);
+}
+
 document.addEventListener('keydown',function createItem() {
-    var input = document.getElementById('demo').value;
-    localStorage.setItem('server', input);
-    });
-    
-    window.onload = function readValue() {
-    document.getElementById('demo').innerHTML = localStorage.getItem('server');
-    }
+var input = document.getElementById('demo').value;
+localStorage.setItem('server', input);
+});
+
+window.onload = function readValue() {
+document.getElementById('demo').innerHTML = localStorage.getItem('server');
+};
+
+
+
