fe37a4ef4ade7b1962d179e3bf45912f2db4f6cc
Jacob Fillpot <admin@Jacobs-MacBook-Pro.local>
2019-12-13 20:51:20 -0600

    added event listener

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-document.getElementById('demo').onkeydown = function createItem() {
+document.addEventListener('keydown',function createItem() {
     var input = document.getElementById('demo').value;
     localStorage.setItem('server', input);
-    }
+    });
     
     window.onload = function readValue() {
     document.getElementById('demo').innerHTML = localStorage.getItem('server');
