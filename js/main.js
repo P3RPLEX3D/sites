9d2046f392b5f9a2b8dc8b2be3312893f46423b7
Jacob Fillpot <admin@Jacobs-MacBook-Pro.local>
2019-12-16 01:39:01 -0600

    setup

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
-let yay = Math.floor(Math.random() * 6) + 1;
-console.log(yay);
+
+	window.onload(localStorage.getItem('token', 'key'));
+
+
+	function allowDrop(ev) {
+		ev.preventDefault();
+	}
+
+	function drag(ev) {
+		ev.dataTransfer.setData("text", ev.target.id);
+	}
+
+	function drop(ev) {
+		localStorage.setItem("token", 'key');
+		ev.preventDefault();
+		var data = ev.dataTransfer.getData("text");
+		ev.target.appendChild(document.getElementById(data));
+	}
