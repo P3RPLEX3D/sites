7c9358da72f3ad3a80c5749100a621e8b58391ec
Jacob Fillpot <admin@Jacobs-MacBook-Pro.local>
2019-12-28 18:23:40 -0600

    test

--- js/main.js
+++
@@ -1,6 +0,0 @@
-window.onload = function readValue() {
-	document.getElementById("input").innerHTML = localStorage.getItem("server");
-};
-window.onkeypress = function() {
-    localStorage.setItem('server');
-}
