0eca939fbd426fea228f857d64a2c0f4fd2a523c
Jacob Fillpot <admin@Jacobs-MacBook-Pro.local>
2020-07-10 21:28:34 -0500

    done

---
+++ js/main.js
@@ -0,0 +1,199 @@
+window.addEventListener('click', event);
+
+let b = document.getElementById('Athletics');
+b.onclick = function() {
+    let y = document.getElementById('AthleticsB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    console.log('this works')
+}
+
+let c = document.getElementById('Acrobatics');
+c.onclick = function() {
+    let y = document.getElementById('AcrobaticsB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    console.log('this works')
+}
+
+let d = document.getElementById('SleightOfHand');
+d.onclick = function() {
+    let y = document.getElementById('SleightOfHandB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    console.log('this works')
+}
+
+let e = document.getElementById('Stealth');
+e.onclick = function() {
+    let y = document.getElementById('StealthB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    console.log('this works')
+}
+
+let f = document.getElementById('Arcana');
+f.onclick = function() {
+    let y = document.getElementById('ArcanaB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    console.log('this works')
+}
+
+let g = document.getElementById('History');
+g.onclick = function() {
+    let y = document.getElementById('HistoryB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    console.log('this works')
+}
+
+let h = document.getElementById('Investigation');
+h.onclick = function() {
+    let y = document.getElementById('InvestigationB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    console.log('this works')
+}
+
+let i = document.getElementById('Nature');
+i.onclick = function() {
+    let y = document.getElementById('NatureB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    console.log('this works')
+}
+
+let j = document.getElementById('Religion');
+j.onclick = function() {
+    let y = document.getElementById('ReligionB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    console.log('this works')
+}
+
+let k = document.getElementById('AnimalHandling');
+k.onclick = function() {
+    let y = document.getElementById('AnimalHandlingB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    console.log('this works')
+}
+
+let l = document.getElementById('Insight');
+l.onclick = function() {
+    let y = document.getElementById('InsightB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    console.log('this works')
+}
+
+let m = document.getElementById('Medicine');
+m.onclick = function() {
+    let y = document.getElementById('MedicineB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    
+}
+
+let n = document.getElementById('Perception');
+n.onclick = function() {
+    let y = document.getElementById('PerceptionB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    
+}
+
+let o = document.getElementById('Survival');
+o.onclick = function() {
+    let y = document.getElementById('SurvivalB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    
+}
+
+let p = document.getElementById('Deception');
+p.onclick = function() {
+    let y = document.getElementById('DeceptionB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    
+}
+
+let q = document.getElementById('Intimidation');
+q.onclick = function() {
+    let y = document.getElementById('IntimidationB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    
+}
+
+let r = document.getElementById('Performance');
+r.onclick = function() {
+    let y = document.getElementById('PerformanceB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    
+}
+
+let s = document.getElementById('Persuasion');
+s.onclick = function() {
+    let y = document.getElementById('PersuasionB');
+    if(y.style.display === 'flex') {
+        y.style.display = 'none';
+    }else {
+        y.style.display = 'flex';
+    }
+    
+}
